97d6384a962c210f041037793f155b05d81507ce
kimdogyeom <kdg412@naver.com>
2024-06-24 17:53:49 +0900

    kimdogyeom

---
+++ Project/frontend/src/views/Admin/AdminMember/AdminInquiry.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="container-fluid px-4">
+      <ol class="breadcrumb mb-4">
+        <li class="breadcrumb-item active">관리자</li>
+      </ol>
+      <div class="card mb-4">
+            <div class="card-header">
+                <i class="fas fa-table me-1"></i>
+                1:1문의 리스트
+            </div>
+            <div class="card-body">
+                <DataTableComponent :data="inquiryList" :columns="inquiryColumns" />
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+import DataTableComponent from '@/components/Admin/DataTableComponent.vue'
+import axios from 'axios';
+
+export default {
+    components: {
+        DataTableComponent
+    },
+    data(){
+        return{
+            inquiryList: [],
+            inquiryColumns: ['문의제목','회원ID','작성일','답변상태'],
+        }
+    },
+    created() {},
+    methods: {},
+}
+
+</script>
---
+++ Project/frontend/src/views/Admin/AdminProduct/AdminProdList.vue
@@ -0,0 +1,3 @@
+<template>
+    <div>하위</div>
+</template>
