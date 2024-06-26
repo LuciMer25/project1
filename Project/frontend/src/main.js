775db5fd6c16f8f260800289977d83a00822923c
LuciMer25 <1997@seokwon.com>
2024-06-26 12:00:35 +0900

    main

--- Project/frontend/src/main.js
+++ Project/frontend/src/main.js
@@ -1,12 +1,32 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+// import store from './store.js'
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap"
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import "startbootstrap-sb-admin/dist/css/styles.css"
 import "simple-datatables/dist/style.css"
 
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+
+
+const vuetify = createVuetify({
+  components,
+  directives,
+})
+
+
 createApp(App)
 .use(router)
+// .use(store)
+.use(vuetify)
 .mount('#app')
+
+
+
