d8842fe38893be2fa38d756041a7340795d31e09
ssh <thd8172@naver.com>
2024-06-26 16:54:48 +0900

    (login, song)

--- Project/frontend/src/views/Main/login/signup/signtUp1.vue
+++ Project/frontend/src/views/Main/login/signup/signtUp1.vue
@@ -18,6 +18,7 @@
             src="https://k.kakaocdn.net/14/dn/btroDszwNrM/I6efHub1SN5KCJqLm1Ovx1/o.jpg"
             width="222"
             alt="카카오 로그인 버튼"
+            
           />
         </a>
       </div>
