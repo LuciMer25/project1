1f18234a41b4b43725e9aa4f8f5307d19f5d4f6d
MaengSeonWoo <msw0808@naver.com>
2024-06-27 17:28:37 +0900

    dd

--- Project/backend/mysql/sql.js
+++ Project/backend/mysql/sql.js
@@ -1,28 +1,25 @@
-const memberSql = require('./memberSql.js');
+const memberSql = require("./memberSql.js");
 
-const adminOrder = require('./admin/adminOrder');
-const categorySql = require('./main/categorySql.js');
-const adminBoard = require('./admin/adminBoard')
-const adminMember = require('./admin/adminMember')
-const adminProduct = require('./admin/adminProduct')
-const inquirysql = require('./mypage/inquirysql')
-const qnasql = require('./mypage/qnasql');
-const mypageorder = require('./mypage/mypageorder');
-const productsql = require('./product/productSql.js')
-
+const adminOrder = require("./admin/adminOrder");
+const categorySql = require("./main/categorySql.js");
+const adminBoard = require("./admin/adminBoard");
+const adminMember = require("./admin/adminMember");
+const adminProduct = require("./admin/adminProduct");
+const inquirysql = require("./mypage/inquirysql");
+const qnasql = require("./mypage/qnasql");
+const mypageorder = require("./mypage/mypageorder");
+const productsql = require("./product/productSql.js");
 
 module.exports = {
-   ...adminOrder,
-    ...adminBoard,
-    ...adminOrder,
-    ...adminMember,
-    ...adminProduct,
-
-
-   ...memberSql,
-   ...inquirysql,
-   ...qnasql,
-   ...mypageorder,
-   ...productsql,
-   ...categorySql
-}
+  ...adminOrder,
+  ...adminBoard,
+  ...adminOrder,
+  ...adminMember,
+  ...adminProduct,
+  ...memberSql,
+  ...inquirysql,
+  ...qnasql,
+  ...mypageorder,
+  ...productsql,
+  ...categorySql,
+};
