67b2aa189bc697f70d62ba695a427a2a4ecc776b
zbvmf530 <zbvmf530@gmail.com>
2024-07-05 16:35:11 +0900

    homeview에 테스트메소드 제거

--- Project/frontend/src/views/Main/HomeView.vue
+++ Project/frontend/src/views/Main/HomeView.vue
@@ -24,9 +24,7 @@
 </template>
 
 <script>
-// @ is an alias to /src
 
-import axios from "axios";
 import NewProductList from "../../components/product/NewProductList.vue";
 import BestSellList from "../../components/product/BestSellList.vue";
 import ThreeMinuteList from "../../components/product/ThreeMinuteList.vue";
@@ -41,22 +39,6 @@ export default {
     CupbapList,
     MDsPickList
   },
-  methods:{
-    gotoOrderPage(){
-
-      let array = [
-      { prod_no: 1, prod_name: "진라면 매운맛 (120GX5)", prod_cnt: 3, price:3580, order_amount: 10740, prod_img: "test.png" },
-      { prod_no: 2, prod_name: "열라면 (120GX5)", prod_cnt: 1, price: 3580, order_amount: 3580, prod_img: "test.png" },
-      { prod_no: 3, prod_name: "진라면 순한맛 (120GX5)", prod_cnt: 4, price: 3580, order_amount: 14320, prod_img: "test.png" },
-
-      ];
-      this.$store.dispatch('updateItemList', array);
-      this.$router.push({ name: 'order' });
-    },
-    gotoProductPage(){
-      this.$router.push(`product/1`);
-    }
-  }
 }
 </script>
 
