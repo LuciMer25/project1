d3529f9304262a6d18c340d7d207eee42bc60448
LuciMer25 <1997@seokwon.com>
2024-07-05 12:23:17 +0900

    seokwon 7.5

--- Project/frontend/src/components/main/Header3.vue
+++ Project/frontend/src/components/main/Header3.vue
@@ -102,7 +102,7 @@
           <li><RouterLink to="/notify">공지사항</RouterLink></li>
           <li><RouterLink to="/QnAList">QnA</RouterLink></li>
           <li><RouterLink to="/inquiryList">1:1문의</RouterLink></li>
-          <li><RouterLink to="">베스트</RouterLink></li>
+          <li><RouterLink to="/bestproductlist">베스트</RouterLink></li>
         </ul>
       </div>  
     </header>
